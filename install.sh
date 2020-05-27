4523f2b1bc55a5d48358c50a4f6b006fdee85923
Jeremy Helms <digitaljhelms@gmail.com>
2020-05-26 22:51:57 -0500

    Adds install todo

---
+++ install.sh
@@ -0,0 +1,10 @@
+# todo: write an automation script for initial dotfile setup
+#  1. create ~/.bash_local
+#  2. symlink .bash_local/* to ~/.bash_local/
+#  3. create ~/.bash_functions
+#  4. symlink .bash_functions/* to ~/.bash_functions/
+#  5. symlink .bash_aliases to ~/.bash_aliases
+#  6. symlink .bash_profile to ~/.bash_profile
+#  7. symlink .zshrc to ~/.zshrc
+#  8. symlink .bashrc to ~/.bashrc
+#  9. symlink .hyper.js to ~/.hyper.js
